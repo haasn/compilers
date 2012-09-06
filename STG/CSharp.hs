d50fc84df76bdecc574422790f2816f419171a2e
nand <nand@lavabit.com>
2012-09-06 17:19:53 +0200

    Move to single stack and transition to partial class with static members
    Ignore-this: 281f840f513cfa56d0713b9cb698ec11
    
    darcs-hash:20120906151953-cd4f6-96519babc2acc1c9746c32627740abd65bbbaeba

--- STG/CSharp.hs
+++ STG/CSharp.hs
@@ -20,8 +20,15 @@ runGen = DL.toList . execWriter . (`runReaderT` 0)
 putProgram :: Program -> Gen ()
 putProgram Program{..} = do
   preamble
-  putBindings bindings
+  putGlobal bindings
   epilogue
+
+putGlobal :: [Binding] -> Gen ()
+putGlobal = mapM_ $ \b -> do
+  put ("static Fun _" ++ lhs b ++ " = new Fun (delegate {")
+  indent (putLF (lhs b) (rhs b))
+  put "});"
+  br
 
 putBindings :: [Binding] -> Gen ()
 putBindings bs = do
@@ -30,23 +37,24 @@ putBindings bs = do
   mapM_ putBinding bs
 
 putBinding :: Binding -> Gen ()
-putBinding (Binding n LF{..}) = do
+putBinding (Binding n lf) = do
   put ("_" ++ n ++ ".f = delegate {")
-
-  indent $ do
-    when upd (putUpdate n)
-
-    forM_ args $ \a -> put ("var _" ++ a ++ " = args.Pop ();")
-    unless (null args) br
-
-    putExpr body
-
+  indent (putLF n lf)
   put "};"
   br
 
+putLF :: Name -> LambdaForm -> Gen ()
+putLF n LF{..} = do
+  when upd (putUpdate n)
+
+  forM_ args $ \a -> put ("var _" ++ a ++ " = stack.Pop ();")
+  unless (null args) br
+
+  putExpr body
+
 putExpr :: Expr -> Gen ()
 putExpr (App n as) = do
-  forM_ (reverse as) $ \a -> put ("args.Push (_" ++ a ++ ");")
+  forM_ (reverse as) $ \a -> put ("stack.Push (_" ++ a ++ ");")
   put ("return _" ++ n ++ ";")
 
 putExpr (LetRec bs e) = putBindings bs >> putExpr e
@@ -58,10 +66,10 @@ putExpr (Constr t ns) = do
     n -> put ("vars = new Fun[" ++ show n ++ "];")
   let putV v n = put ("vars[" ++ show n ++ "] = (_" ++ v ++ ");")
   zipWithM_ putV ns [0..]
-  put "return cont.Pop ();"
+  put "return stack.Pop ();"
 
 putExpr (Case e ms d) = do
-  put "cont.Push (new Fun (delegate {"
+  put "stack.Push (new Fun (delegate {"
   indent $ do
     put "switch (tag) {"
     indent $ do
@@ -87,7 +95,7 @@ putMatch Match{..} = do
 
 putUpdate :: Name -> Gen ()
 putUpdate n = do
-  put "cont.Push (new Fun (delegate {"
+  put "stack.Push (new Fun (delegate {"
   indent $ do
     put "var mytag  = tag;"
     put "var myvars = vars;"
@@ -95,9 +103,9 @@ putUpdate n = do
     indent $ do
       put "tag  = mytag;"
       put "vars = myvars;"
-      put "return cont.Pop ();"
+      put "return stack.Pop ();"
     put "};"
-    put "return cont.Pop ();"
+    put "return stack.Pop ();"
   put "}));"
   br
 
@@ -112,30 +120,26 @@ preamble = do
   put "  public Fun (FunPtr p) { f = p; }}"
   put "delegate Fun FunPtr ();"
   br
-  put "class STG { static void Main() {"
+  put "partial class STG {"
   br
-  put "int tag = 0;"
-  br
-  put "var args = new Stack<Fun> ();"
-  put "var cont = new Stack<Fun> ();"
-  put "Fun[] vars = null;"
+  put "static int tag = 0;"
+  put "static Stack<Fun> stack = new Stack<Fun> ();"
+  put "static Fun[] vars = null;"
   br
 
 epilogue :: Gen ()
 epilogue = do
-  put "var output = new Fun ();"
-  put "output.f = delegate {"
-  put "  Console.WriteLine (tag);"
-  put "  Environment.Exit (0);"
-  put "  return null;"
-  put "};"
-  br
-  put "cont.Push (output);"
-  br
-  put "var next = _main;"
-  put "while (true)"
-  put "  next = next.f ();"
-  br
+  put "static void Main () {"
+  indent $ do
+    put "stack.Push (new Fun (delegate {"
+    put "  Console.WriteLine (tag);"
+    put "  Environment.Exit (0);"
+    put "  return null;"
+    put "}));"
+    br
+    put "var next = _main;"
+    put "while (true)"
+    put "  next = next.f ();"
   put "}}"
 
 -- Helpers and minor functions
