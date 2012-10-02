86e98a01380dbc00b17c10ec32ab667b1043b850
nand <nand@lavabit.com>
2012-10-02 20:48:27 +0200

    Minor code prettification
    Ignore-this: f9f408ac2c4347fc8df3685f55573e6c
    
    darcs-hash:20121002184827-cd4f6-33c71add935cc43e95649affa82079896881552d

--- STG/CSharp.hs
+++ STG/CSharp.hs
@@ -10,14 +10,14 @@ import Control.Monad.Reader
 import Control.Monad.Writer
 import qualified Data.DList as DL
 
-type Gen = ReaderT Int (Writer (DL.DList Char))
+type Gen = ReaderT Int (Writer (DL.DList Char)) ()
 
-runGen :: Gen () -> String
+runGen :: Gen -> String
 runGen = DL.toList . execWriter . (`runReaderT` 0)
 
 -- Code generators
 
-putProgram :: Program -> Gen ()
+putProgram :: Program -> Gen
 putProgram Program{..} = do
   put "using System;"
   put "using System.Collections.Generic;"
@@ -26,27 +26,21 @@ putProgram Program{..} = do
   indent (putGlobal bindings)
   put "}"
 
-putGlobal :: [Binding] -> Gen ()
+putGlobal :: [Binding] -> Gen
 putGlobal = mapM_ $ \(n, b) -> do
   put("static Fun _" ++ n ++ " =")
   indent $ putExpr b
   put ";"
   br
 
-putBindings :: [Binding] -> Gen ()
-putBindings bs = do
-  mapM_ alloc bs
-  br
-  mapM_ putBinding bs
-
-putBinding :: Binding -> Gen ()
+putBinding :: Binding -> Gen
 putBinding (n, b) = do
   put("_" ++ n ++ ".f = delegate {")
   returns (putExpr b)
   put "};"
   br
 
-putExpr :: Expr -> Gen ()
+putExpr :: Expr -> Gen
 putExpr (App n []) = put $ '_' : n
 
 putExpr (App n xs) = scoped $ do
@@ -63,7 +57,11 @@ putExpr (Lambda ns e) = scoped $ do
 
   returns $ putExpr e
 
-putExpr (LetRec bs e) = scoped $ putBindings bs >> returns (putExpr e)
+putExpr (LetRec bs e) = scoped $ do
+  forM_ bs $ \(n,_) -> put ("var _" ++ n ++ " = new Fun ();")
+  br
+  mapM_ putBinding bs
+  returns $ putExpr e
 
 putExpr (Prim op a b) = scoped $ do
   put (show op ++ " (")
@@ -76,27 +74,27 @@ putExpr (Literal i) = put $ "lit (" ++ show i ++ ")"
 
 -- Helpers and minor functions
 
-alloc :: Binding -> Gen ()
+alloc :: Binding -> Gen
 alloc (n, _) = put ("var _" ++ n ++ " = new Fun ();")
 
-scoped :: Gen () -> Gen ()
+scoped :: Gen -> Gen
 scoped a = do
   put "new Fun (delegate {"
   indent a
   put "})"
 
-returns :: Gen () -> Gen ()
+returns :: Gen -> Gen
 returns a = put "return" >> indent a >> put ";"
 
-indent :: Gen a -> Gen a
+indent :: Gen -> Gen
 indent = local (+2)
 
-put :: String -> Gen ()
+put :: String -> Gen
 put s = do
   i <- ask
   tell $ DL.fromList (replicate i ' ')
   tell $ DL.fromList s
   br
 
-br :: Gen ()
+br :: Gen
 br = tell $ DL.singleton '\n'
